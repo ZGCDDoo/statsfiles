82e0d8d2e6d578ede71d6fc5fa09275a2a1d1754
Charles-David Hebert <hebertcharlesdavid@outlook.com>
2018-12-17 17:27:48 -0500

    Removed unecessat files.

--- statsfiles/__main__.py
+++ statsfiles/__main__.py
@@ -2,12 +2,13 @@ from statsfiles import statsfiles
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Do the stats of files.')
+parser = argparse.ArgumentParser(
+    description='Do the statistics of tabular like files.')
 parser.add_argument('iter_start',  type=int,
-                    help='iteration start for the stats')
+                    help='iteration start for performing the statistics')
 
 parser.add_argument('--afm', action='store_const', const=True,
-                    help='afm is present')
+                    help='Antiferromagnetism is present')
 
 args = parser.parse_args()
 
